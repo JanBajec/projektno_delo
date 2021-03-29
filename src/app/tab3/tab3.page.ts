9b9c04bfda78061a8622115070d89e431a0558d5
Tomaz26 <tomazklobucar13@gmail.com>
2021-03-29 06:16:51 +0100

    Dodal sem približek izgleda tab3, popraviti moram še izbiro kalorij, trudim se da bi bila narejena kot scroll izbira. Dodati moram še izpis skupnih kalorij ter izpis imena prijavljenega

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
+  customYearValues = [0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000];
+
   constructor() {}
 
 }

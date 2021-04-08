1be9d6e9e6dc14ebfb1db49da9011bf918cc61b8
Jan Bajec <48645043+JanFeriAccount10@users.noreply.github.com>
2021-04-08 02:07:50 +0200

    Auth Guard and some litle changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBLgJ5Smlee0RYRnLg7XjGkrRj-l0o2QLc',
+    authDomain: 'projektno-delo.firebaseapp.com',
+    projectId: 'projektno-delo',
+    storageBucket: 'projektno-delo.appspot.com',
+    messagingSenderId: '878000184430',
+    appId: '1:878000184430:web:e099e7726e52363ab89a7f',
+    measurementId: 'G-8MJE5THD6L'
+  }
 };
 
 /*
